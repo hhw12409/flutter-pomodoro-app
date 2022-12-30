0da5c71ad64c59215f6099b3a8049169aec2d5ec
hhw12409 <sksk2425@naver.com>
2022-12-30 16:26:25 +0900

    feat: styling Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_app/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,11 +19,7 @@ class MyApp extends StatelessWidget {
         cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Hello world',
-          ),
-        ),
+        body: HomeScreen(),
       ),
     );
   }
